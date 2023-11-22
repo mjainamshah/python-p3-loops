3497f018cbfeec034c1782b2ff2a217a24bb390a
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-22 16:31:52 +0300

    COMPLETED

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # SOLUTION!
+     i = 10
+     while i > 0:
+        print(i)
+        if i == 1:
+            print('Happy New Year!')
+        i -= 1
+     pass
 
 def square_integers(int_list):
-    # code goes here!
+    # SOLUTION!
+    squared_numbers = ([num ** 2 for num in int_list])
+    return(squared_numbers)
     pass
 
 def fizzbuzz():
-    # code goes here!
+    # SOLUTION!
+    for num in range (1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
